fe1a6a3356d9b0acb07c5b56cea5c0e5baf9a1f8
Jordan Bhar <bharjor@sheridancollege.ca>
2022-02-15 00:28:26 -0500

    Commit 3: Implemented camera_page.dart containing Sample code from fluter cookbook

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
+
+import 'camera_page.dart';
+
 
 
 
@@ -44,8 +45,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-  void onTakePhoto() {
+  Future<void> onTakePhoto() async {
+    // Ensure that plugin services are initialized so that `availableCameras()`
+    // can be called before `runApp()`
+    WidgetsFlutterBinding.ensureInitialized();
 
+    // Obtain a list of the available cameras on the device.
+    final cameras = await availableCameras();
+
+    // Get a specific camera from the list of available cameras.
+    final firstCamera = cameras.first;
+
+    runApp(
+      MaterialApp(
+        theme: ThemeData.dark(),
+        home: TakePictureScreen(
+          // Pass the appropriate camera to the TakePictureScreen widget.
+          camera: firstCamera,
+        ),
+      ),
+    );
   }
 
   void onResetPhoto(){
