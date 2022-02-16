3f678f2fa93251245d9edf5f26fdc06a8daa74d9
Jordan Bhar <bharjor@sheridancollege.ca>
2022-02-15 23:32:17 -0500

    Commit 5: Implemented Reseting photo to defualt no image photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -82,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    const noImageDisplayed = 'assets/noimage.png';
+
 
     return Scaffold(
       backgroundColor: const Color(0xFFFFFFFF),
@@ -119,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                    onPressed: null,
+                    onPressed: onResetPhoto,
                     child: const Text("Reset Picture"),
                     style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(Colors.blue),
